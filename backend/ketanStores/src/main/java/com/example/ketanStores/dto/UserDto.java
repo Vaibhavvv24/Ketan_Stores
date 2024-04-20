b357175c184f4e87fdbedc35d1085fff966508ff
valmikGit <quantumcomputingiiitb@gmail.com>
2024-04-20 17:20:32 +0530

    Created UserDto, UserEntity, UserRepo.

--- backend/ketanStores/src/main/java/com/example/ketanStores/dto/UserDto.java
+++ backend/ketanStores/src/main/java/com/example/ketanStores/dto/UserDto.java
@@ -1,2 +1,20 @@
-package com.example.ketanStores.dto;public class UserDto {
+package com.example.ketanStores.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private Long id;
+    private String username;
+    private String emailId;
+    private String password;
 }
