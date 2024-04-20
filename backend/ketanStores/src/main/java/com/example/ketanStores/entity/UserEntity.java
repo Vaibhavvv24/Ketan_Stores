a8036c9a2c51760ea03c5614f567270ae25c8a4c
Vaibhav <mittalvaibhav277@gmail.com>
2024-04-20 17:40:01 +0530

    added jwt auth filter

--- backend/ketanStores/src/main/java/com/example/ketanStores/entity/UserEntity.java
+++ backend/ketanStores/src/main/java/com/example/ketanStores/entity/UserEntity.java
@@ -8,19 +8,57 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(
             strategy = GenerationType.AUTO
     )
     private Long id;
-    private String username;
     private String emailId;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return emailId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- backend/ketanStores/src/main/java/com/example/ketanStores/repository/UserRepo.java
+++ backend/ketanStores/src/main/java/com/example/ketanStores/repository/UserRepo.java
@@ -1,15 +1,12 @@
 package com.example.ketanStores.repository;
 
 import com.example.ketanStores.entity.UserEntity;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.FluentQuery;
+
 
 import java.util.Optional;
-import java.util.function.Function;
 
 public interface UserRepo extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findById(Long id);
-    Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmailId (String emailId);
 }
