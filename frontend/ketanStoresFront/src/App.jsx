e0b3adef9aabb118c475d7eb1af203be54b715d4
pratham-chawdhry <122348596+pratham-chawdhry@users.noreply.github.com>
2024-04-20 23:24:39 +0530

    Page changes

--- frontend/ketanStoresFront/src/App.jsx
+++ frontend/ketanStoresFront/src/App.jsx
@@ -5,6 +5,12 @@ import LoginFinal from "./pages/Login";
 import { StyledEngineProvider, CssVarsProvider } from "@mui/joy/styles";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
+import KetanStores from "./pages/Page-Filters/KetanStores";
+import Men from "./pages/Page-Filters/Men";
+import Kurta from "./pages/Page-Filters/kurta";
+import Chudidar from "./pages/Page-Filters/chudidar";
+import Silk from "./pages/Page-Filters/silk";
+import Cotton from "./pages/Page-Filters/cotton";
 
 const App = () => {
   return (
@@ -14,6 +20,12 @@ const App = () => {
           <Routes>
             <Route path='/login' element={<LoginFinal />} />
             <Route path='/' element={<Home />} />
+            <Route exact path='/ketan-stores' element={<KetanStores />} />
+            <Route exact path='/ketan-stores/men' element={<Men />} />
+            <Route exact path="/ketan-stores/men/kurta" element={<Kurta/>} />
+            <Route exact path="/ketan-stores/men/chudidar" element={<Chudidar/>} />
+            <Route exact path="/ketan-stores/men/kurta/silk" element={<Silk/>} />
+            <Route exact path="/ketan-stores/men/kurta/cotton" element={<Cotton/>} />
           </Routes>
         </CssVarsProvider>
       </StyledEngineProvider>
--- frontend/ketanStoresFront/src/pages/Home.jsx
+++ frontend/ketanStoresFront/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import KetanStores from './Page-Filters/KetanStores'
 
 const Home = () => {
     return (
     <>
         <Navbar />
+        <KetanStores />
         <Footer/>
             
     </>
