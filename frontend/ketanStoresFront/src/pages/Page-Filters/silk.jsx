f80e289b190550699f6bf4a83738d240ae5e199e
pratham-chawdhry <122348596+pratham-chawdhry@users.noreply.github.com>
2024-04-23 11:24:33 +0530

    utftku

--- frontend/ketanStoresFront/src/pages/Page-Filters/silk.jsx
+++ frontend/ketanStoresFront/src/pages/Page-Filters/silk.jsx
@@ -1,28 +1,121 @@
-import React from 'react';
-import Card from '@mui/joy/Card';
-import Typography from '@mui/joy/Typography';
-import AspectRatio from '@mui/joy/AspectRatio';
+/** @format */
+
+import React from "react";
+import { Link } from "react-router-dom";
+import { useColorScheme } from "@mui/joy/styles";
+import Sheet from "@mui/joy/Sheet";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Typography from "@mui/joy/Typography";
+import FormControl from "@mui/joy/FormControl";
 import FormLabel from "@mui/joy/FormLabel";
+import Input from "@mui/joy/Input";
+import Button from "@mui/joy/Button";
+import RadioGroup from "@mui/material/RadioGroup";
+import Radio from "@mui/material/Radio";
+import Select from "@mui/joy/Select";
+import Option from "@mui/joy/Option";
 
 export default function Silk() {
   return (
     <div>
-      <Card className='flex-row justify-evenly items-center' sx={{ width: 550, height : 230}}>
-        <div>
-          <img
-            src='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286'
-            srcSet='https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x'
-            loading='lazy'
-            alt=''
-            className='w-[100%] h-[150px] object-cover rounded-md border border-2 border-gray-200 shadow-md'
-          />
-        </div>
-        <div>
-          <Typography level='h6' component='h1' className='flex items-center justify-left h-[22px]'>
-              <FormLabel className='text-xl pl-2'>Yosemite National Park</FormLabel> 
-          </Typography>
-        </div>
-      </Card>
+      <main>
+        <CssBaseline />
+        <Sheet
+          sx={{
+            width: 660,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            mx: "auto", // margin left & right
+            my: 4, // margin top & bottom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
+            gap: 2,
+            borderRadius: "sm",
+            boxShadow: "md",
+          }}
+          variant='outlined'
+        >
+          <div>
+            <Typography level='h4' component='h1'>
+              <b>Filters:</b>
+            </Typography>
+            <Typography level='body-sm'>Choose appropriate items using available filters:</Typography>
+          </div>
+          <div className='flex justify-evenly h-full w-full mt-2'>
+          <FormControl>
+            <div className='flex justify-evenly h-full w-full'>
+              <div className='flex-col justify-left pl-1 items-center gap-2 mt-2'>
+                <Typography
+                  level='h6'
+                  component='h1'
+                  className='flex items-center h-[22px]'
+                >
+                  <span className='text-xs'>1.</span>
+                  <FormLabel className='pl-2'>Size:</FormLabel>
+                </Typography>
+                <div className='flex justify-center items-center w-full mt-2'>
+                  <Input type='text' placeholder='Enter Size' style = {{width: 170}}/>
+                </div>
+              </div>
+            </div>
+          </FormControl>
+          <FormControl>
+            <div className='flex justify-evenly h-full w-full'>
+              <div className='flex-col justify-left pl-1 items-center gap-2 mt-2'>
+                <Typography
+                  level='h6'
+                  component='h1'
+                  className='flex items-center h-[22px]'
+                >
+                  <span className='text-xs'>2.</span>
+                  <FormLabel className='pl-2'>Colour:</FormLabel>
+                </Typography>
+                <div className='flex justify-center items-center w-full mt-2'>
+                  <Input type='text' placeholder='Enter Colour' style = {{width: 170}} />
+                </div>
+              </div>
+            </div>
+          </FormControl>
+          <FormControl>
+            <div className='flex justify-evenly h-full w-full'>
+              <div className='flex-col justify-left pl-1 items-center gap-2 mt-2'>
+                <Typography
+                  level='h6'
+                  component='h1'
+                  className='flex items-center h-[22px]'
+                >
+                  <span className='text-xs'>3.</span>
+                  <FormLabel className='pl-2'>Type:</FormLabel>
+                </Typography>
+                <div className='flex justify-center items-center w-full mt-2'>
+                <Select defaultValue='Plain' style = {{width: 170}}>
+                    <Option
+                      value='Plain'
+                      onClick={(e) => {
+                        setType("Plain");
+                      }}
+                    >
+                      Plain
+                    </Option>
+                    <Option
+                      value='Print & Design'
+                      onClick={(e) => {
+                        setType("PRINT_AND_DESIGN");
+                      }}
+                    >
+                      Print & Design
+                    </Option>
+                  </Select>
+                </div>
+              </div>
+            </div>
+          </FormControl>
+          </div>
+        </Sheet>
+      </main>
     </div>
   );
 }
+
+//Size, colour, type
