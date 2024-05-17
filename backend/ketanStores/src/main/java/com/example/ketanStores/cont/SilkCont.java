0d66718e23296b58ec3fb49246e6eb9c70a2fbe8
valmikGit <quantumcomputingiiitb@gmail.com>
2024-05-17 12:46:47 +0530

    added logger to CottonCont, Cotton_serviceimp

--- backend/ketanStores/src/main/java/com/example/ketanStores/cont/SilkCont.java
+++ backend/ketanStores/src/main/java/com/example/ketanStores/cont/SilkCont.java
@@ -2,6 +2,7 @@ package com.example.ketanStores.cont;
 import com.example.ketanStores.dto.PutBody;
 import com.example.ketanStores.enums.KurtaEnum;
 import com.example.ketanStores.enums.SilkEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.ketanStores.dto.Silk_dto;
@@ -20,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.sql.rowset.serial.SerialBlob;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/kurta_silk")
 @CrossOrigin(origins = "http://localhost:5173")
@@ -28,43 +30,64 @@ public class SilkCont {
     public SilkCont(Silk_service silk_service) {
         this.silk_service = silk_service;
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<?> getClothbyid(@PathVariable("id") Long id) {
+        log.info("SilkCont: Received a request to get silk with id = {}", id);
         Silk_dto silk = silk_service.getClothbyid(id);
         if (silk == null) {
+            log.error("SilkCont: Silk with id = {} was not found.", id);
             return ResponseEntity.status(404).body("No data found");
         }
+        log.info("SilkCont: Found silk = {}", silk);
         return ResponseEntity.ok().body(silk);
     }
+
     @GetMapping("/all")
     public ResponseEntity<?> getall() {
+        log.info("SilkCont: Received a request to get all silks");
         ArrayList<Silk_dto> silk = silk_service.getall();
+        log.info("SilkCont: Returning all the silks.");
         return ResponseEntity.ok().body(silk);
     }
+
     @GetMapping("/type/{type}")
     public ResponseEntity<?> getbytype(@PathVariable("type") String type) {
+        log.info("SilkCont: Received a request to retrieve silks wrt type-filter, type = {}", type);
         ArrayList<Silk_dto> silk = silk_service.getbytype(type);
+        log.info("SilkCont: Returning filtered silks.");
         return ResponseEntity.ok().body(silk);
     }
+
     @GetMapping("/size/{size}")
     public ResponseEntity<?> getbysize(@PathVariable("size") int size) {
+        log.info("SilkCont: Received a request to retrieve silks wrt size-filter, size = {}", size);
         ArrayList<Silk_dto> silk = silk_service.getbysize(size);
+        log.info("SilkCont: Returning the filtered silks.");
         return ResponseEntity.ok().body(silk);
     }
+
     @PostMapping("/silk")
     public ResponseEntity<?> postSilk(@RequestParam("name") String name, @RequestParam("img") MultipartFile file, @RequestParam("price") int price, @RequestParam("quantity") int quantity, @RequestParam("type") String type, @RequestParam("size") int size, @RequestParam("colour") String colour) throws IOException, SQLException {
+        log.info("SilkCont: Received a request to post silk with parameters: name = {}, price = {}, quantity = {}, type = {}, size = {}, colour = {}",
+                name, price, quantity, type, size, colour);
         byte[] bytes = file.getBytes();
         Blob blob = new SerialBlob(bytes);
         SilkEnum silkEnum = SilkEnum.valueOf(type);
         KurtaEnum kurtaEnum = KurtaEnum.SILK;
         Silk_dto silk_dto = silk_service.createSilk(name, price, quantity, silkEnum, blob, size, colour, kurtaEnum);
         if (silk_dto == null) {
+            log.error("SilkCont: Error posting silk with parameters: name = {}, price = {}, quantity = {}, type = {}," +
+                    "size = {}, colour = {}", name, price, quantity, type, size, colour);
             return ResponseEntity.notFound().build();
         }
+        log.info("SilkCont: Posted silk = {}", silk_dto);
         return ResponseEntity.ok().body(silk_dto);
     }
+
     @GetMapping("/silk/search/{name}")
     public ResponseEntity<?> search(@PathVariable String name) {
+        log.info("SilkCont: Received a request to ");
         System.out.println(name);
         List<Silk_dto> silk_dtos = silk_service.getSilkByName(name);
         System.out.println(silk_dtos);
